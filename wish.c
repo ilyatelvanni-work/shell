5664842be543f76e2d08a6159130be06ab81b3eb
ILgotin <ilgotin@intermedia@com>
2023-01-11 13:56:22 +0300

    Program repeatedly prints wish> as a user prompt

--- wish.c
+++ wish.c
@@ -9,9 +9,9 @@
 
 int main(int argc, char *argv[]) {
   
-  const char exit[] = "exit";
+  // const char exit[] = "exit";
   int id = 1;
-  int error_code;
+  // int error_code;
 
   char *line = NULL;
   size_t len = 0;
@@ -21,52 +21,52 @@ int main(int argc, char *argv[]) {
     printf("wish> ");
     getline(&line, &len, stdin);
 
-    if (strcmp(exit, line) == 0) {
-      break;
-    } else {
-      printf("%i", strcmp(exit, line));
-      id = fork();
+  //   if (strcmp(exit, line) == 0) {
+  //     break;
+  //   } else {
+  //     printf("%i", strcmp(exit, line));
+  //     id = fork();
 
-      if (id > 0) {
-        waitpid(id, NULL, 0);
-      } else if (id == 0) {
+  //     if (id > 0) {
+  //       waitpid(id, NULL, 0);
+  //     } else if (id == 0) {
 
-        //build-in_programms
-        //exit -> exit(0)
-        //cd <arg?> -> chdir(<args?>)
-        //path <args> -> set path of the shell (instead
-        //   of /bin /usr/bin)
+  //       //build-in_programms
+  //       //exit -> exit(0)
+  //       //cd <arg?> -> chdir(<args?>)
+  //       //path <args> -> set path of the shell (instead
+  //       //   of /bin /usr/bin)
 
-        // commands-in-path
-        // check bin
-        // access("/bin/ls", X_OK)
-        // /usr/bin/l
+  //       // commands-in-path
+  //       // check bin
+  //       // access("/bin/ls", X_OK)
+  //       // /usr/bin/l
 
-        // redirection
-        // > print nothing on the screen print in file
-        // (overwrite)
-        // no redirection for build ups
-        // no redirection chains
+  //       // redirection
+  //       // > print nothing on the screen print in file
+  //       // (overwrite)
+  //       // no redirection for build ups
+  //       // no redirection chains
 
-        // parallel commands
-        // & execute commands without waiting one-by-one
-        // but execute waiting whem simultaneously exec
+  //       // parallel commands
+  //       // & execute commands without waiting one-by-one
+  //       // but execute waiting whem simultaneously exec
 
-        // char error_message[30] = "An error has occurred\n";
-        // write(STDERR_FILENO, error_message, strlen(error_message)); 
-        // CATCH ALL SYNTAX ERRORS EITHER
+  //       // char error_message[30] = "An error has occurred\n";
+  //       // write(STDERR_FILENO, error_message, strlen(error_message)); 
+  //       // CATCH ALL SYNTAX ERRORS EITHER
 
 
 
-        error_code = 1; // execv(line);
-      } else {
-        error_code = -1;
-      }
-      if (error_code) {
-        printf("ERROR, %i\n", error_code);
-        return id;
-      }
-    }
+  //       error_code = 1; // execv(line);
+  //     } else {
+  //       error_code = -1;
+  //     }
+  //     if (error_code) {
+  //       printf("ERROR, %i\n", error_code);
+  //       return id;
+  //     }
+  //   }
   }
 
   return 0;
